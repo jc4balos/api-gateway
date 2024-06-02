463feb212e3208face0fcdb7859aec1c60c38ff4
Jc Abalos <jc4balos@gmail.com>
2024-06-03 06:39:22 +0800

    added default files

---
+++ src/test/java/com/jc4balos/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc4balos.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
